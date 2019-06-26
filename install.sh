9e15d3ed1e76796a143529bf3a8061055205683f
Eduardo Henrique <eduardo-hnq@hotmail.com>
2019-06-26 10:57:09 -0300

    Update install.sh
    Automatizando script *

--- install.sh
+++ install.sh
@@ -14,43 +14,20 @@ getenforce
 sed -i 's/=enable/=disabled/' /etc/selinux/config
 hostnamectl set-hostname controller
 yum install -y ntpdate
-
 #Install OpenStack in CentOS
 yum install -y centos-release-openstack-rocky
 yum update -y
 yum install -y openstack-packstack
-yum install -y bash-completion vim epel-release
-
-yum install -y wget
-
-
-
-#SELINUX=disable
-
-packstack --gen-answer-file='ntp'.con
-lsf
-
-
-vi 13.04.16.conf
-
-CONFIG_NTP_SERVERS=0.ro.pool.ntp.org
-CONFIG_PROVISION_DEMO=n
-CONFIG_KEYSTONE_ADMIN_PW=your_password
-CONFIG_HORIZON_SSL=y
-CONFIG_MARIADB_PW=mypassword1234
-CONFIG_NAGIOS_PW=nagios1234
-
-vi /etc/ssh/sshd_config
-#PermitRootLogin yes
+#yum install -y bash-completion vim epel-release
+#yum install -y wget
+packstack --gen-answer-file='CONFIGURACAO'.conf
+sed -i 's/CONFIG_NTP_SERVERS=/CONFIG_NTP_SERVERS=0.ro.pool.ntp.org #/' /root/controller/CONFIGURACAO.conf
+sed -i 's/CONFIG_PROVISION_DEMO=/CONFIG_PROVISION_DEMO=n #/' /root/controller/CONFIGURACAO.conf
+sed -i 's/CONFIG_KEYSTONE_ADMIN_PW=/CONFIG_KEYSTONE_ADMIN_PW=SENHA #/' /root/controller/CONFIGURACAO.conf
+sed -i 's/CONFIG_HORIZON_SSL=/CONFIG_HORIZON_SSL=y #/' /root/controller/CONFIGURACAO.conf
+sed -i 's/CONFIG_MARIADB_PW=/CONFIG_MARIADB_PW=SENHA #/' /root/controller/CONFIGURACAO.conf
+#sed -i 's/CONFIG_NAGIOS_PW=nagios1234 #/' /root/controller/CONFIGURACAO.conf
+sed -i 's/PermitRootLogin yes #/' /etc/ssh/sshd_config
 systemctl restart sshd
-systemctl status sshd
-
-packstack --answer-file ntp.conf
-
-vim /etc/httpd/conf.d/ssl.conf
-#Listen 443 https
-
-systemctl restart httpd.service
-
-packstack --answer-file /root/ntp.conf
-https://192.168.1.40/dashboard 
+#systemctl status sshd
+packstack --answer-file CONFIGURACAO.conf
